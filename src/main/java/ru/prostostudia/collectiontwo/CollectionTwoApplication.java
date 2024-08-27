e6e4c0ab91a17d264f50f2dd362c8e6689ab6719
Roman Kuzmin <ghelatin@mail.ru>
2024-08-27 12:05:41 +0300

    commit init

---
+++ src/main/java/ru/prostostudia/collectiontwo/CollectionTwoApplication.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.collectiontwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionTwoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/prostostudia/collectiontwo/CollectionTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.prostostudia.collectiontwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
